9851f7f2e6acd506dbc3331b1621b0e0ab7b10ba
mikuoyamada <miku44xx@gmail.com>
2024-03-26 21:38:32 +0800

    levelcheck

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('level');
-// });
+Route::get('/', function () {
+    return view('level');
+});
 
 Auth::routes();
 
