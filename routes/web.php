ce977e68b2b93e88f384786a361549b246ed9792
Leak1219 <leak121219@icloud.com>
2024-03-28 16:54:31 +0800

    変更しました

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\IndexController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,9 +27,9 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/index', 'IndexController@index')->name('index');
+// Route::get('/index', 'IndexController@index')->name('index');
 
-Route::get('/index', 'App\Http\Controllers\IndexController@index')->name('index');
+Route::get('/index', [IndexController::class, 'index'])->name('index.index');
 
 // Route::get('/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
 // Route::post('/register', 'Auth\RegisterController@register');
