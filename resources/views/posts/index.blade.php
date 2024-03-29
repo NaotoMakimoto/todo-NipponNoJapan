8d68258da6092f933651d73bbe7be07cf61a3096
NaotoMakimoto <makimaki2510@gmail.com>
2024-03-30 08:15:20 +0900

    Update index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -104,7 +104,7 @@
 
         function revealTodosAtSpecificTime() {
     const revealTime = new Date();
-    revealTime.setHours(22, 12, 30, 0); // 次の表示時刻を設定
+    revealTime.setHours(8, 12, 30, 0); // 次の表示時刻を設定
 
     if (new Date() > revealTime) {
         revealTime.setDate(revealTime.getDate() + 1); // 現在が指定時刻を過ぎていたら翌日に設定
