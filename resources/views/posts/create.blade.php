36c9cfc8c5aa82b212755601698af29354bbb526
mikuoyamada <miku44xx@gmail.com>
2024-03-29 22:44:41 +0800

    Update create.blade.php

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -30,15 +30,16 @@
         </div>
     </div>
   </div>
-  {{-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script> --}}
 
   <script>
     document.getElementById('todoForm').addEventListener('submit', function(event) {
         var titleInput = document.querySelector('input[name="title"]');
+  
+        var titleValue = titleInput.value.trim();
+
         if (!titleInput.value.trim()) {
             alert('新規TODOを記入してください');
-            event.preventDefault(); // フォームの送信をキャンセル
+            event.preventDefault(); 
         }
     });
   </script>
