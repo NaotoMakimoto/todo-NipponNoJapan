6b78ecc2772f6f11abccc43e40d68cb308905239
NaotoMakimoto <makimaki2510@gmail.com>
2024-04-02 02:59:30 +0900

    いったんストップ

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,54 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/index_style.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <style>
-        .custom-card {
-            /* カードの幅を調整 */
-            width: 20rem;
-            height: 28rem; /* 仮の高さ。画像のアスペクト比を維持するために設定 */
-            display: flex; /* 子要素を縦方向に配置するために flex を使用 */
-            justify-content: center; /* 子要素を中央に配置 */
-            align-items: center; /* 子要素を中央に配置 */
-        }
-    
-        .card-img-container {
-            /* 画像をカードの中央に配置し、オーバーフローした部分を隠す */
-            overflow: hidden;
-            width: 100%; /* 画像を親要素に合わせる */
-            height: 100%; /* 画像を親要素に合わせる */
-        }
-    
-        .card-img-top {
-            object-fit: cover; /* 画像を維持しながら親要素にフィットさせる */
-            width: 100%; /* 画像を親要素に合わせる */
-            height: 100%; /* 画像を親要素に合わせる */
-        }
-
-        .postimg{
-            width: 20rem;
-            height: 20rem; 
-        }
-    </style>
-    
-
 </head>
+
+@extends('layouts.app_original')
+@section('content')
 <body>
 
     @if(Session::has('success'))
     <div class="alert alert-success">
         {{ Session::get('success') }}
     </div>
-@endif
+    @endif
 
-    <h1>日々のTODOを記録</h1>
+    <h1>TODO diary</h1>
 
     <form action="" method="POST" enctype="multipart/form-data">
         @csrf
         {{-- <input type="file" name="image"> --}}
         <input type="file" name="image" accept="image/*">
-        <button type="submit" class="btn btn-primary">アップロード</button>
+        <button type="submit" class="btn btn-primary">＋</button>
     </form>
 
     <div class="row row-cols-1 row-cols-md-4 g-4 ">
@@ -79,7 +55,7 @@
     <form action="{{ route('todo.index') }}" method="GET">
         <button type="submit" class="btn btn-primary">戻る</button>
     </form>
-
+@endsection
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
