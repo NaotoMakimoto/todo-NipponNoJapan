2b2d694fe0bdd1ae6dad7ee3903927a83b354a0d
Leak1219 <leak121219@icloud.com>
2024-04-02 19:59:24 +0800

    ログアウト完了

--- resources/views/layouts/app_original.blade.php
+++ resources/views/layouts/app_original.blade.php
@@ -1,6 +1,12 @@
 <body>
   <header class="">
     <h2><a href="{{ route('level') }}">Lv.{{ $level }}</a></h2>
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+      @csrf
+    </form>
+    <a onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+      ログアウト
+    </a>
   </header>
   @yield('content')
   <footer>
