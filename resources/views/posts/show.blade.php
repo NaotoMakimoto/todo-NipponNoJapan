540a2b36c3897823db3b0ab97efe57fdcc32d241
NaotoMakimoto <makimaki2510@gmail.com>
2024-04-03 11:50:46 +0900

    変えた

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/show_style.css') }}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"> --}}
     <style>
         body {
             background-color: #E7EEFE;
