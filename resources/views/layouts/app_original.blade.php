0e2c53d81126948bf872c9775f60820ba3a04c33
mikuoyamada <miku44xx@gmail.com>
2024-04-04 11:55:00 +0800

    ロゴ作ったよ＾＾

--- resources/views/layouts/app_original.blade.php
+++ resources/views/layouts/app_original.blade.php
@@ -1,4 +1,4 @@
-<body>
+{{-- <body>
   <header class="">
 
     <p onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
@@ -9,9 +9,32 @@
       @csrf
     </form>
   </header>
+  
+  @yield('content')
+  <footer>
+   
+  </footer>
+</body> --}}
+
+
+<body>
+
+  <header class="">
+    <img src="{{ asset('image/newlogo.png') }}" alt="Logo" class="logo">
+
+    <p onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+      {{ $user->name }}
+    </p>
+    <h2><a href="{{ route('level') }}">Lv.{{ $level }}</a></h2>
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+      @csrf
+    </form>
+  </header>
+  
   @yield('content')
   <footer>
    
   </footer>
 </body>
+
 
